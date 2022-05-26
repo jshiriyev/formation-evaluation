47285144b14ec2f3d286b5e716dd0e0467ac8a3d
Javid Shiriyev <shiriyevcavid@gmail.com>
2022-05-26 16:46:55 +0400

    improved logview

--- scrollmlib.py
+++ scrollmlib.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 
+from tkinter import ttk
+
 from matplotlib import gridspec
 from matplotlib import pyplot as plt
 
@@ -16,37 +18,96 @@ import numpy as np
 
 class LogView():
 
+	lineColors = (
+		"black",
+		"crimson",
+		"blue",
+		"sienna",
+		)
+
+	spinerelpos = (0,0.1,0.2,0.3)
+
 	def __init__(self,root):
 
-		self.frame = tk.Frame(root)
+		self.root = root
+
+		self.root.title("RockPy")
+
+		self.frame = ttk.Frame(root)
 		self.frame.pack(side=tk.TOP,fill=tk.BOTH,expand=1)
 
-		self.figure = plt.figure(figsize=(4,64))
+		self.canvas = tk.Canvas(self.frame)
+		self.canvas.pack(side=tk.LEFT,fill=tk.BOTH,expand=1)
 
-		axes_num = 1
+		self.scrollbar = ttk.Scrollbar(self.frame) #,orient=tk.VERTICAL
+		self.scrollbar.pack(side=tk.LEFT,fill=tk.Y)
 
-		self.grids = gridspec.GridSpec(1,axes_num)
+		self.canvas.config(yscrollcommand=self.scrollbar.set)
+		self.scrollbar.config(command=self.canvas.yview)
+
+		self.canvas.bind_all("<MouseWheel>",self._on_mousewheel)
+
+	def set_figure(self,numaxes=1,winch=3,hinch=128,dpi=100.):
+
+		zmult = int((500-0)/20.)
+
+		self.spinepos = [1+x/zmult for x in self.spinerelpos]
+
+		self.figure = plt.figure(figsize=(winch*numaxes,hinch),dpi=dpi)
+
+		self.grids = gridspec.GridSpec(1,numaxes)
 		self.grids.update(wspace=0)
 
-		axis0 = plt.subplot(self.grids[0])
+		self.figcanvas = FigureCanvasTkAgg(self.figure,self.canvas)
 
-		axis0.set_xticks([])
+		canwin = self.canvas.create_window(0,0,window=self.figcanvas.get_tk_widget(),anchor=tk.constants.NW)
 
-		axis0.grid(True,which="both",axis='y')
+		self.figcanvas.get_tk_widget().config(width=winch*numaxes*dpi,height=hinch*dpi)
 
-		axis = axis0.twiny()
+		self.canvas.itemconfigure(canwin,width=winch*numaxes*dpi,height=hinch*dpi)
+		self.canvas.config(scrollregion=self.canvas.bbox(tk.constants.ALL),width=winch*dpi,height=winch*dpi)
 
+		self.figure.canvas.draw()
 
+		self.figure.set_tight_layout(True)
 
-		xaxis = np.random.random(500)
-		xaxis[-10:] = 0.5
-		yaxis = np.arange(500)
+	def set_axes(self,numsubaxes=None):
 
-		xticks = self.get_xticks(xaxis)
-		yticks = self.get_yticks(yaxis)
+		if numsubaxes is None:
+			numsubaxes = (1,)*len(self.grids)
 
-		axis.plot(xaxis,yaxis)
-		##axis.figure.set_size_inches(4,400)
+		self.axes = []
+
+		for idaxis,(numsubaxis,grid) in enumerate(zip(numsubaxes,self.grids)):
+
+			axisM = plt.subplot(grid)
+
+			axisM.set_xticks([])
+
+			axisM.grid(True,which="both",axis='y')
+
+			for tic in axisM.yaxis.get_major_ticks():
+				if idaxis>0:
+					tic.label1.set_visible(False)
+				tic.tick1line.set_visible(False)
+
+			axisS = [axisM.twiny() for _ in range(numsubaxis)]
+
+			for axis in axisS:
+				axis.set_xticks([0,1])
+				plt.setp(axis.xaxis.get_majorticklabels()[0],ha="left")
+				plt.setp(axis.xaxis.get_majorticklabels()[-1],ha="right")
+
+			self.axes.append(axisS)
+
+	def set_axis(self,idaxis,idline,xvals,yvals):
+
+		axis = self.axes[idaxis][idline]
+
+		axis.plot(xvals,yvals,color=self.lineColors[idline])
+
+		xticks = self.get_xticks(xvals)
+		yticks = self.get_yticks(yvals)
 
 		axis.set_xlim([xticks.min(),xticks.max()])
 		axis.set_xticks(xticks)
@@ -54,15 +115,23 @@ class LogView():
 		axis.set_ylim([yticks.max(),yticks.min()])
 		axis.set_yticks(yticks)
 
-		axis.grid(True,which="major",axis='x')
 		axis.grid(True,which="both",axis='y')
 
 		axis.yaxis.set_minor_locator(AutoMinorLocator(10))
 
-		# if axdict["ptype"][indexJ] is None:
+		if idaxis>0:
+			for tic in axis.yaxis.get_minor_ticks():
+				tic.tick1line.set_visible(False)
+
+		if idline==0:
+			axis.grid(True,which="major",axis='x')
+		else:
+			axis.spines["top"].set_position(("axes",self.spinepos[idline]))
+			axis.spines["top"].set_color(self.lineColors[idline])
+			axis.tick_params(axis='x',color=self.lineColors[idline],labelcolor=self.lineColors[idline])
+
 		axis.xaxis.set_major_formatter(ScalarFormatter())
-		# elif axdict["ptype"][indexJ]=="log":
-		# 	self.axes[indexI].subax[indexJ].xaxis.set_major_formatter(LogFormatter())
+		# axis.xaxis.set_major_formatter(LogFormatter())
 
 		majorTicksX = axis.xaxis.get_major_ticks()
 
@@ -78,43 +147,6 @@ class LogView():
 
 		plt.setp(axis.xaxis.get_majorticklabels()[0],ha="left")
 		plt.setp(axis.xaxis.get_majorticklabels()[-1],ha="right")
-
-
-
-
-
-
-		self.canvas = tk.Canvas(self.frame)
-		self.canvas.pack(side=tk.LEFT,fill=tk.BOTH,expand=1)
-
-		scrollbar = tk.Scrollbar(self.frame) #,orient=tk.VERTICAL
-		scrollbar.pack(side=tk.LEFT,fill=tk.Y)
-
-		self.canvas.config(yscrollcommand=scrollbar.set)
-		scrollbar.config(command=self.canvas.yview)
-
-		self.canvas.bind_all("<MouseWheel>",self._on_mousewheel)
-
-		figagg = FigureCanvasTkAgg(self.figure,self.canvas)
-		canagg = figagg.get_tk_widget()
-
-		##figagg.get_tk_widget().config(width=400,height=1600)
-		##figagg.get_tk_widget().config(bg="blue",scrollregion=(0,0,400,1600))
-		##figagg.get_tk_widget().config(yscrollcommand=scrollbar.set)
-		##figagg.get_tk_widget().pack(side=tk.LEFT,fill=tk.BOTH,expand=tk.Y)
-
-		canwin = self.canvas.create_window(0,0,window=canagg,anchor=tk.constants.NW)
-
-		wi,hi = [i*self.figure.dpi for i in self.figure.get_size_inches()]
-
-		canagg.config(width=wi,height=hi)
-
-		self.canvas.itemconfigure(canwin,width=wi,height=hi)
-		self.canvas.config(scrollregion=self.canvas.bbox(tk.constants.ALL),width=400,height=400)
-
-		self.figure.canvas.draw()
-
-		self.figure.set_tight_layout(True)
 
 	def get_xticks(self,xvals,xmin=None,xmax=None,xscale="normal",xdelta=None,xdelta_count=11):
 
@@ -203,5 +235,14 @@ if __name__ == "__main__":
 	root = tk.Tk()
 
 	las = LogView(root)
+
+	Y = np.arange(500)
+	X = np.random.random(500)
+
+	las.set_figure(3)
+	las.set_axes((1,2,1))
+	las.set_axis(0,0,xvals=X,yvals=Y)
+	las.set_axis(1,0,xvals=X,yvals=Y)
+	las.set_axis(1,1,xvals=np.random.random(500),yvals=Y)
 
 	tk.mainloop()
